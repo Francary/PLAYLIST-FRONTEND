b1783ad8ca490dc9e762091fa2595ea073a8d91f
Francary <francarymolinapaez@gmail.com>
2023-11-22 23:52:40 -0300

    14-Clase mejora del CSS HomePage y PlayListPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,13 @@ const HomePage = () => {
     // if (!auth) return <div>LOGING ....---</div>
     return (
         <>
-        <div className="container-fluid d-f p-3 ">
-        <h1>HOME PAGE</h1>
-        <p className="w-75">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro animi a, iste delectus quae quasi dignissimos numquam deserunt neque distinctio in ducimus enim repellat totam autem modi quod eaque dolor.</p>
-        <Link  className="btn btn-success" to={"/playlist"}> Go to Playlist</Link>
-        <button className="btn btn-success" onClick={logout}>Logout</button>
+        <div className="container d-flex flex-column p-3 gap-4 justify-content-center mt-5" >
+        <h1 className="text-center">HOME PAGE</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro animi a, iste delectus quae quasi dignissimos numquam deserunt neque distinctio in ducimus enim repellat totam autem modi quod eaque dolor.</p>
+        <div className=" d-flex gap-4 justify-content-center">
+            <button className="btn btn-success" onClick={logout}>Logout</button>
+            <Link  className="btn btn-success" to={"/playlist"}> Go to Playlist</Link>
+        </div>
         </div>
         </>
     )
--- src/pages/PlayListPage.jsx
+++ src/pages/PlayListPage.jsx
@@ -1,8 +1,8 @@
 const PlayListPage = () => {
     return (
-        <div>  
-            <h1>PlayListPage </h1>
-            <div className="w-100 d-flex flex-row gap-4">
+        <div className="container d-flex flex-column p-3 gap-4 justify-content-center mt-5">  
+            <h1 className="text-center">PlayListPage </h1>
+            <div className="d-flex flex-row gap-4">
                 <button className="btn btn-success">Create</button>
                 <input type="search" name="" id="" placeholder="Search" className="form-control"/>
             </div>
